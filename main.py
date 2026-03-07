efb7e0d33378743c9b395c53b9729bb1644d9db5
Marcos Eduardo <marcos@nouva.se>
2026-03-07 15:28:46 -0300

    Git ignore and new stufs to config, main and scraping, just testing some filters
    Signed-off-by: Marcos Eduardo <marcos@nouva.se>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 async def main():
     crawler = GCPDocsCrawler()
     for endpoint in crawler.gcp_services:
-       await crawler.crawl_gpc_docs(endpoint)
+        await crawler.crawl_gpc_docs(endpoint["endpoint"])
 
 if __name__ == "__main__":
     asyncio.run(main())
--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,4 +1,3 @@
-import asyncio
 from crawl4ai import *
 from config.config import Config
 
@@ -9,7 +8,25 @@ class GCPDocsCrawler:
         self.gcp_services = config.GCP_SERVICES
 
     async def crawl_gpc_docs(self, endpoint: str):
-        service_doc = f"{self.base_url}/{endpoint}"
+        service_url = f"{self.base_url}/{endpoint}"
+
+        run_config = CrawlerRunConfig(
+            excluded_tags=['li', 'lu', 'picture'],
+            exclude_domains=['https://docs.cloud.google.com', 'https://console.cloud.google.com/freetrial?hl=pt-br','https://docs.cloud.google.com/free/docs/free-cloud-features?hl=pt-br#free-tier'],
+            deep_crawl_strategy=BFSDeepCrawlStrategy(
+                max_depth=2,
+                max_pages=50
+            ),
+            markdown_generator = DefaultMarkdownGenerator(
+                content_filter=PruningContentFilter(threshold=0.5)
+            )
+        )
+
         async with AsyncWebCrawler() as crawler:
-            result = await crawler.arun(url=service_doc)
-            print(result.markdown)
+            results = await crawler.arun(url=service_url, config=run_config)
+
+            for result in results:
+                if result.url == 'https://docs.cloud.google.com' or result.url == 'https://docs.cloud.google.com/talent-solution/job-search/docs?hl=pt-br' or result.url == 'https://docs.cloud.google.com/solutions/media-entertainment?hl=pt-br' or result.url == 'ttps://docs.cloud.google.com/telecom-subscriber-insights/docs?hl=pt-br' or  result.url == 'https://console.cloud.google.com/freetrial?hl=pt-br' or result.url == 'https://console.cloud.google.com?hl=pt-br' or result.url == 'https://docs.cloud.google.com/docs?hl=pt-br' or result.url == 'https://docs.cloud.google.com/docs/cross-product-overviews?hl=pt-br' or result.url == 'https://docs.cloud.google.com/free/docs/free-cloud-features?hl=pt-br':
+                    continue
+                print(f"URL: {result.url}")
+                #print(result.markdown.fit_markdown)
