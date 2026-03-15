ccffa3bc904cc42848dfbd9da3e2bd77ebdad3c6
Marcos Eduardo <marcos@nouva.se>
2026-03-15 20:54:36 -0300

    New class Ingestion with Qdrant client and embedding model for ingestion process. and added ingestion.run() method to complete the process.
    also att for requirements
    
    Signed-off-by: Marcos Eduardo <marcos@nouva.se>

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from scraping.scraping import GCPDocsCrawler
+from rag.ingestion import Ingestion
 import asyncio
 
 async def main():
     crawler = GCPDocsCrawler()
+    ingestion = Ingestion()
     for service in crawler.gcp_services:
         try:
             print(f"\n Crawling: {service['product']}")
             await crawler.crawl_gpc_docs(service)
         except Exception as e:
                 print(f"Error crawling {service['product']}: {e}")
+                continue
 
+    print("Crawling process completed successfully.")
+    ingestion.run()
 if __name__ == "__main__":
     asyncio.run(main())
